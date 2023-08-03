fcc7ed4addf4dd384b3c791b0c5c36634a002294
nizar <nizar@nizars-Air.fritz.box>
2023-08-03 23:28:18 +0200

    h

--- src/app/components/social/social.jsx
+++ src/app/components/social/social.jsx
@@ -1,24 +1,24 @@
 import React from 'react'
 import styles from './styles.module.css'
 
-import {TiSocialLinkedin} from "react-icons/ti"
+/* import {TiSocialLinkedin} from "react-icons/ti" */
 import {BsGithub } from "react-icons/bs"
-import {BiLogoInstagram } from "react-icons/bi"
+/* import {BiLogoInstagram } from "react-icons/bi" */
 
 
 const social = () => {
   return (
     <div className={styles.container}>
         
-     <span className={styles.singlerIcon}>
+    {/*  <span className={styles.singlerIcon}>
      <TiSocialLinkedin />
-     </span>
+     </span> */}
      <span className={styles.singlerIcon}>
      <BsGithub />
      </span>
-     <span className={styles.singlerIcon}>
+ {/*     <span className={styles.singlerIcon}>
      <BiLogoInstagram />
-     </span>
+     </span> */}
     </div>
   )
 }
