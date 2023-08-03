674de5c3e8ad78f22018f1786dffd462fed6c704
nizar <nizar@nizars-Air.fritz.box>
2023-08-03 23:13:40 +0200

    u

--- src/app/components/slider/slider.jsx
+++ src/app/components/slider/slider.jsx
@@ -5,6 +5,7 @@ import {useState , useRef ,useEffect} from 'react'
 
 
 import {IoIosArrowForward , IoIosArrowBack} from 'react-icons/io'
+
 import classNames from 'classnames'
 
 const Slider = () => {
