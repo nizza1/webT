6c7d864ffc04a30bb4042d2744980f6d72c04077
nizar <nizar@nizars-Air.fritz.box>
2023-08-11 15:15:34 +0200

    h

--- src/app/components/tes/tes.jsx
+++ src/app/components/tes/tes.jsx
@@ -3,34 +3,34 @@
 import React, { useState, useEffect } from 'react';
 import styles from './styles.module.css';
 import Lottie from 'lottie-react';
-import { motion , useViewportScroll} from 'framer-motion';
 import myAnimation from '@app/animations/animation_ll5965nn.json';
 
+import { motion, useScroll } from "framer-motion"
+
 const Tes = () => {
-    const animationData = myAnimation;
-    const [animationProgress, setAnimationProgress] = useState(0);
-    const { scrollY } = useViewportScroll();
-  
-    const handleScroll = () => {
-      const normalizedProgress = scrollY.get() / scrollRange;
-      setAnimationProgress(normalizedProgress);
-    };
-  
-    // Set the scroll range based on the content height
-    const scrollRange = document.documentElement.scrollHeight - window.innerHeight;
+
+    const { scrollYProgress } = useScroll();
   
     return (
       <div className={styles.container}>
         test
-  
-        <motion.div onScroll={handleScroll} className={styles.scrollContainer}>
-          {/* Render Lottie animation only on the client side */}
-          {typeof window !== 'undefined' && (
-            <Lottie animationData={animationData} progress={animationProgress} />
-          )}
-        </motion.div>
+        <motion.div 
+        className={styles.bar}>
+
+        <div className={styles.barContainer}>
+        <motion.div style={{ scaleX: scrollYProgress }} 
+        className={styles.barT}/>
+        </div>
+
+         </motion.div > 
+
       </div>
     );
   };
   
   export default Tes;
+
+
+  /* 
+  <Lottie animationData={animationData} progress={animationProgress} />
+  */
