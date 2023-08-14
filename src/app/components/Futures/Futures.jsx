56ddfa2e6e660a04c4bd071d2dc1fa24de91965a
nizar <nizar@nizars-Air.fritz.box>
2023-08-14 21:35:47 +0200

    d

--- src/app/components/Futures/Futures.jsx
+++ src/app/components/Futures/Futures.jsx
@@ -8,7 +8,7 @@ const Futures = () => {
 
     const texts = [ 
         {
-            title:' UI & UX-Design',
+            title:' Individuelles Design',
             text: 'Wir konzentrieren uns darauf, visuell ansprechende und intuitive Benutzeroberflächen zu erstellen, die Ihr Publikum von dem Moment an fesseln, in dem es auf Ihre Website gelangt.' ,
             text2: ' Durch die Kombination aktueller Design-Trends mit den Bedürfnissen Ihrer Benutzer entwickeln wir einzigartige und konkurrenzfähige UI & UX-Designs, die Sie von Ihren Mitbewerbern abheben lassen.',
             id : '1'
@@ -19,7 +19,7 @@ const Futures = () => {
             id : '2'
          }, */
          {
-            title:'Responsive auf allen Geräten',
+            title:'Responsives Design auf allen Geräten',
             text: 'Wir gewährleisten, dass Ihre Website auf allen Geräten, von Desktops bis hin zu Mobilgeräten eine außergewöhnliche Benutzererfahrung bietet.',
              text2: 'Unsere mobilfähigen, responsiven Designs passen sich automatisch an verschiedene Bildschirmgrößen und Auflösungen an und bieten ein konsistentes und ansprechendes Erlebnis für jeden Benutzer.' ,
             id : '2'
@@ -31,7 +31,7 @@ const Futures = () => {
             id : '3'
          },
          {
-            title:'Verbesserte Sichtbarkeit',
+            title:'Mehr Sichtbarkeit',
             text: 'Das Erstellen einer ästhetisch ansprechenden und funktionalen Website ist wichtig, aber ebenso wichtig ist es sicherzustellen, dass Ihre Zielgruppe sie leicht findet. ' ,
             text2: 'Unsere SEO-Dienstleistungen zielen darauf ab, die Suchmaschinensichtbarkeit und den organischen Traffic Ihrer Website zu verbessern.',
             id : '4'
