7434166c6e24ea9fc6e065b98b2ec82e2eb937b2
nizar <nizar@nizars-MacBook-Air.local>
2023-10-02 16:52:05 +0200

    h

--- src/app/components/contact/contact-data/contactData.jsx
+++ src/app/components/contact/contact-data/contactData.jsx
@@ -1,19 +1,25 @@
 import React from 'react'
 import {FiPhoneCall} from 'react-icons/fi'
 import {MdAlternateEmail} from 'react-icons/md'
+import {BsFillPinMapFill} from 'react-icons/bs'
 
 import styles from './styles.module.css'
 const ContactData = () => {
   return (
     <div className={styles.container}>
+      <div>
+            <BsFillPinMapFill className={styles.icons} />
+            <p >Schnieglinger Str. 264, 90427 Nürnberg</p>
+        </div>
+
         <div>
             <FiPhoneCall className={styles.icons} />
-            <a href="tel:017684070282">+49 1768 4070 282</a>
+            <a href="tel:+49015770390746">+49 157 703 907 46</a>
         </div>
 
         <div>
             <MdAlternateEmail className={styles.icons}/>
-            <a href = "mailto: nizardjassim@gmail.com">nizardjassim@gmail.com</a>
+            <a href = "mailto: c.bohdan@nuancestudio.de">c.bohdan@nuancestudio.de</a>
         </div>
 
     </div>
