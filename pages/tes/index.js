92b475799f5fa52f8be4f8e49e839ff2dfcf3257
nizar <nizar@nizars-Air.fritz.box>
2023-10-04 21:28:30 +0200

    k

--- pages/tes/index.js
+++ pages/tes/index.js
@@ -4,38 +4,12 @@ import React from 'react'
 // test components 
 import Tes from '@app/components/tes/tes'
 import styles from './styles.module.css'
+import Sliderr from '@app/components/newSlider/sliderr'
 const index = () => {
   return (
     <div className={styles.container}>
       <div className={styles.innerContainer}>
-        <div className={styles.slider}>
-          <div className={styles.elem}>
-            <h2>1</h2>
-          </div>
-
-          <div className={styles.elem}>
-            <h2>2</h2>
-          </div>
-
-          <div className={styles.elem}>
-            <h2>3</h2>
-          </div>
-
-          <div className={styles.elem}>
-            <h2>4</h2>
-          </div>
-          <div className={styles.elem}>
-            <h2>5</h2>
-          </div>
-
-          <div className={styles.elem}>
-            <h2>6</h2>
-          </div>
-
-          <div className={styles.elem}>
-            <h2>7</h2>
-          </div>
-        </div>
+        <Sliderr />        
       </div>
     </div>
   )
---
+++ src/app/components/newSlider/sliderr.jsx
@@ -0,0 +1,43 @@
+import React , {useRef} from 'react'
+import styles from './styles.module.css'
+import {useScroll , useTransform} from 'framer-motion'
+const sliderr = () => {
+
+    const container = useRef(null);
+/*     const {scrollYProgress} = useScroll({ 
+        target: container ,
+        offset: ['start', 'end'], ['end', 'start']
+    }) */
+
+    
+  return (
+    <div className={styles.container} ref={container}>
+        <div className={styles.slider}>
+            <div className={styles.elem}>
+                <h1>1</h1>
+            </div>
+            <div className={styles.elem}>
+                <h1>2</h1>
+            </div>
+            <div className={styles.elem}>
+                <h1>3</h1>
+            </div>
+            <div className={styles.elem}>
+                <h1>4</h1>
+            </div>
+            <div className={styles.elem}>
+                <h1>5</h1>
+            </div>
+            <div className={styles.elem}>
+                <h1>6</h1>
+            </div>
+            <div className={styles.elem}>
+                <h1>7</h1>
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default sliderr
