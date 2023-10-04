c87a911de3ffd6e3ea6c2791afd138ce4e3835a4
nizar <nizar@nizars-Air.fritz.box>
2023-10-04 22:43:42 +0200

    k

--- src/app/components/newSlider/sliderr.jsx
+++ src/app/components/newSlider/sliderr.jsx
@@ -1,18 +1,26 @@
-import React , {useRef} from 'react'
+import React , {useRef } from 'react'
 import styles from './styles.module.css'
-import {useScroll , useTransform} from 'framer-motion'
+import {useScroll , useTransform , motion} from 'framer-motion'
 const sliderr = () => {
+    const container = useRef(null);
+    const {scrollYProgress} = useScroll({
+        target: container,
+        offset: ["start end" , "end start"]
+    })
 
-   /*  const container = useRef(null); */
-/*     const {scrollYProgress} = useScroll({ 
-        target: container ,
-        offset: ['start', 'end'], ['end', 'start']
-    }) */
+    const x1 = useTransform(scrollYProgress , [0,1], [-450,450])
+
+    const x2 = useTransform(scrollYProgress , [0,1], [450,-450])
+
+
+
 
     
   return (
-    <div className={styles.container}>
-        <div className={styles.slider}>
+    <div className={styles.container} ref={container}>
+        <motion.dev
+        style={{x:x1}}
+         className={styles.slider}>
             <div className={styles.elem}>
                 <h1>1</h1>
             </div>
@@ -35,7 +43,34 @@ const sliderr = () => {
                 <h1>7</h1>
             </div>
 
-        </div>
+        </motion.dev>
+
+        <motion.dev
+        style={{x:x2}}
+         className={styles.slider}>
+            <div className={styles.elem}>
+                <h1>1</h1>
+            </div>
+            <div className={styles.elem}>
+                <h1>2</h1>
+            </div>
+            <div className={styles.elem}>
+                <h1>3</h1>
+            </div>
+            <div className={styles.elem}>
+                <h1>4</h1>
+            </div>
+            <div className={styles.elem}>
+                <h1>5</h1>
+            </div>
+            <div className={styles.elem}>
+                <h1>6</h1>
+            </div>
+            <div className={styles.elem}>
+                <h1>7</h1>
+            </div>
+
+        </motion.dev>
     </div>
   )
 }
