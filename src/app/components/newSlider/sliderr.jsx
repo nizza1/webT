a4db894091e10101da4274fd28482a9d562499f9
nizar <nizar@nizars-Air.fritz.box>
2023-10-06 09:10:46 +0200

    k

--- src/app/components/newSlider/sliderr.jsx
+++ src/app/components/newSlider/sliderr.jsx
@@ -1,5 +1,6 @@
 import React , {useRef } from 'react'
 import styles from './styles.module.css'
+import Image from 'next/image'
 import {useScroll , useTransform , motion} from 'framer-motion'
 const sliderr = () => {
     const container = useRef(null);
@@ -10,67 +11,87 @@ const sliderr = () => {
 
     const x1 = useTransform(scrollYProgress , [0,1], [-450,450])
 
-    const x2 = useTransform(scrollYProgress , [0,1], [450,-450])
-
-
-
+    const x2 = useTransform(scrollYProgress , [0,1], [350,-850])
+    const y2 = useTransform(scrollYProgress , [0,1], [-250,250])
+const refer1 = '/reference/rostockB-min.png'
+const refer2 = '/reference/girafB-min.png'
+const refer2S = '/reference/girafS-min.png'
+const refer1S = '/reference/rostockS-min.png'
+const refer5S = '/reference/comservS-min.png'
+const refer6 = '/reference/conservB-min.png'
+const refer7 = '/reference/horosB-min.png'
+const refer7S = '/reference/horosS-min.png'
+const refer8 = '/reference/imcB-min.png'
+const refer8S = '/reference/imcS-min.png'
+const refer10 = '/reference/quantumB-min.png'
+const refer10S = '/reference/quantumS-min.png'
 
     
   return (
     <div className={styles.container} ref={container}>
         <motion.dev
-        style={{x:x1}}
+        style={{x:x2 , y:y2}}
          className={styles.slider}>
-            <div className={styles.elem}>
-                <h1>1</h1>
+               
+             <div className={styles.elem}>
+                <Image 
+                className={styles.iMG}
+                src={refer2}
+                width={280}
+                height={300}
+                alt='reference'
+                />
             </div>
             <div className={styles.elem}>
-                <h1>2</h1>
+                <Image 
+                className={styles.iMG}
+                src={refer1}
+                width={280}
+                height={300}
+                alt='reference'
+                />
             </div>
             <div className={styles.elem}>
-                <h1>3</h1>
+            <Image 
+                className={styles.iMG}
+                src={refer8}
+                width={280}
+                height={300}
+                alt='reference'
+                />
             </div>
             <div className={styles.elem}>
-                <h1>4</h1>
+            <Image 
+                className={styles.iMG}
+                src={refer10}
+                width={280}
+                height={300}
+                alt='reference'
+                />
             </div>
             <div className={styles.elem}>
-                <h1>5</h1>
+            <Image 
+                className={styles.iMG}
+                src={refer6}
+                width={280}
+                height={300}
+                alt='reference'
+                />
             </div>
             <div className={styles.elem}>
-                <h1>6</h1>
+            <Image 
+                className={styles.iMG}
+                src={refer7}
+                width={280}
+                height={300}
+                alt='reference'
+                />
             </div>
-            <div className={styles.elem}>
-                <h1>7</h1>
-            </div>
+            
+           
 
         </motion.dev>
-
-        <motion.dev
-        style={{x:x2}}
-         className={styles.slider}>
-            <div className={styles.elem}>
-                <h1>1</h1>
-            </div>
-            <div className={styles.elem}>
-                <h1>2</h1>
-            </div>
-            <div className={styles.elem}>
-                <h1>3</h1>
-            </div>
-            <div className={styles.elem}>
-                <h1>4</h1>
-            </div>
-            <div className={styles.elem}>
-                <h1>5</h1>
-            </div>
-            <div className={styles.elem}>
-                <h1>6</h1>
-            </div>
-            <div className={styles.elem}>
-                <h1>7</h1>
-            </div>
-
-        </motion.dev>
+     
     </div>
   )
 }
