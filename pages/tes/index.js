7979a9e2a779dde6933eb85aa0c552ff1a10458d
nizar <nizar@nizars-Air.fritz.box>
2023-10-08 16:44:59 +0200

    ese

--- pages/tes/index.js
+++ pages/tes/index.js
@@ -5,11 +5,33 @@ import React from 'react'
 import Tes from '@app/components/tes/tes'
 import styles from './styles.module.css'
 import Sliderr from '@app/components/newSlider/sliderr'
+
+import Image from 'next/image'
+
+
+
+
+
+import Title from '@app/components/title-gr/title-gr'
+
+
 const index = () => {
   return (
     <div className={styles.container}>
       <div className={styles.innerContainer}>
-        <Sliderr />        
+
+        <div className={styles.titleContainer}>
+       <h1>Design ⚡️ 🧩 🎯〰 🗯️</h1>
+       <h1 className={styles.big}>🎯 🧩 🗯️</h1>
+       <h1>〰〰〰〰〰〰〰</h1>
+
+   
+      
+  
+        </div>
+
+       
+
       </div>
     </div>
   )
