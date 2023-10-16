3343c5e10755639c8e5f12f11f63809913756b01
nizar <nizardjassim@gmail.com>
2023-10-16 11:22:45 +0200

    title

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import '../src/app/globals.css'
 import { Inter } from 'next/font/google'
-
+import Head from 'next/head';
 //components 
 
 //analysis
@@ -11,17 +11,25 @@ import Layout from '@app/components/layout/layout';
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
+/* export const metadata = {
   title: 'Nuance',
   description: 'Professionelle Webentwicklung, Design und Marketing aus Nürnberg'
-}
+} */
 
 
 
 export default function Pages({ Component, pageProps }) {
+  const defaultMetadata = {
+    title: 'Nuance',
+    description: 'Professionelle Webentwicklung, Design und Marketing aus Nürnberg',
+  };
   return (
  
       <>
+       <Head>
+        <title>{defaultMetadata.title}</title>
+        <meta name="description" content={defaultMetadata.description} />
+      </Head>
     
       <Layout className={inter.className}>
          <Component {...pageProps} />
