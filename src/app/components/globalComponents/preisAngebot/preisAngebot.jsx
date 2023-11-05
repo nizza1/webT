ce5f3ab725d15316c6f6f426fc077f724dc567d0
nizar <nizardjassim@gmail.com>
2023-11-05 18:01:31 +0100

    pris angebot

--- src/app/components/globalComponents/preisAngebot/preisAngebot.jsx
+++ src/app/components/globalComponents/preisAngebot/preisAngebot.jsx
@@ -3,9 +3,11 @@
 import React from 'react'
 import styles from './styles.module.css'
 
-import { useRef , useState } from "react";
+import { useRef , useState , useEffect} from "react";
 import { motion, useScroll } from "framer-motion";
 
+import {MdOutlineWorkOutline} from 'react-icons/md'
+import {BsArrowRightSquareFill, BsArrowLeftSquareFill} from 'react-icons/bs'
 
 const preisAngebot = () => {
   
@@ -14,6 +16,7 @@ const preisAngebot = () => {
   const cardRef = useRef(null);
 
   const { scrollXProgress } = useScroll({ container: containerRef });
+  const prozent = parseInt((scrollXProgress.current * 100).toString());
 
   const [scrollPosition, setScrollPosition] = useState(0);
     const [maxScrollPosition, setMaxScrollPosition] = useState(0);
@@ -38,15 +41,21 @@ const preisAngebot = () => {
       setMaxScrollPosition(
         containerRef.current.scrollWidth - containerRef.current.offsetWidth
       );
-      console.log(maxScrollPosition);
+  
     };
+
+    useEffect(() => {
+      setMaxScrollPosition(
+        containerRef.current.scrollWidth - containerRef.current.offsetWidth
+      );
+    }, []);
 
 
   return (
     <div className={styles.container}>
       <div className={styles.header}>
         <div className={styles.circleCo}>
-
+          <div className={styles.prozent}><p>{prozent}%</p></div>
         <svg width="70" height="70" viewBox="0 0 100 100" className={styles.svg}>
         <circle cx="50" cy="50" r="40" pathLength="1" className={styles.svgCircle} />
 
@@ -65,40 +74,229 @@ const preisAngebot = () => {
       <div ref={containerRef} 
       onScroll={handleScroll} 
       className={styles.cBody}>
+
         <div ref={cardRef} className={styles.questionContainer}>
           
           <div className={styles.an}>
-          <label className={styles.label}>
-          In welche Branche sind Sie tätig:
+         
+          <p className={styles.questionText}>In welche Branche sind Sie tätig:</p>
           <input
+          className={styles.textInput}
             type="text"
             name="branche"
            /*  value={formData.branche}
             onChange={handleInputChange} */
+            placeholder='z.B. Unternehmensberatung ....'
           />
-        </label>
+        
           </div>
         </div>
+       
         <div ref={cardRef} className={styles.questionContainer}>
-          <h1>2</h1>
+          <div className={styles.overlay}></div>
+          <div className={styles.an}>
+          
+          <p className={styles.questionText}>Wie groß soll Ihre Website werden:</p>
+       
+          <div className={styles.optionsContainer}>
+            <div>
+            <input type="radio" name="numberOfSite" value="1" id='1op' />
+             <label htmlFor="1op">1</label>
+            </div>
+
+            <div>
+            <input type="radio" name="numberOfSite" value="2-5" id='2-5op'/>
+             <label for="2-5op">2-5</label>
+            </div>
+
+            <div>
+            <input type="radio" name="numberOfSite" value="ab5" id='ab5op'/>
+             <label for="ab5op">ab 5</label>
+            </div>
+          
+
+          </div>
+          </div>
         </div>
+
         <div ref={cardRef} className={styles.questionContainer}>
-          <h1>3</h1>
+          
+          <div className={styles.an}>
+          
+            <p className={styles.questionText}>Wer soll die Betreuung Ihrer Website übernehmen:</p>
+          
+          <div className={styles.optionsContainer}>
+            <div>
+               <input type="radio" name="betreueung" value="Ich-selbst" id='Ich-selbst-op' />
+               <label htmlFor="Ich-selbst-op">Ich selbst</label>
+            </div>
+
+            <div>
+               <input type="radio" name="betreueung" value="Ihre-Agentur"  id='Ihre-Agentur'/>
+               <label htmlFor="Ihre-Agentur">Ihre Agentur</label>
+            </div>
+             
+             
+          </div>
+
+          </div>
         </div>
+
         <div ref={cardRef} className={styles.questionContainer}>
-          <h1>4</h1>
+          
+          <div className={styles.an}>
+          <p className={styles.questionText}>Sind zukünftig Marketingmaßnahmen notwendig?</p>
+
+          <div className={styles.checkBoxContainer}>
+            
+         <div>
+             <input type="checkbox" name="marketing" value="facebook-check" id='facebook-check' />
+            <label htmlFor="facebook-check"> Facebook Ads</label>
+         </div>
+         <div>
+             <input type="checkbox" name="marketing" value="Google-ads" id='Google-ads' />
+            <label htmlFor="Google-ads"> Google Ads</label>
+         </div>
+         <div>
+             <input type="checkbox" name="marketing" value="andere-ch" id='andere-op' />
+            <label htmlFor="andere-op"> andere</label>
+         </div>
+        
+            
+
+
+          </div>
+          </div>
         </div>
+
+        <div ref={cardRef} className={styles.questionContainer}>
+          
+          <div className={styles.an}>
+          <p className={styles.questionText}>Welche Ziele möchten Sie mit Ihrer Website erreichen?</p>
+
+          <div className={styles.checkBoxContainer}>
+            
+         <div>
+             <input type="checkbox" name="ziele-der-website" value="kunden-gewinnen" id='kunden-gewinnen' />
+            <label htmlFor="kunden-gewinnen"> Kunden gewinnen / Umsatz steigern</label>
+         </div>
+         <div>
+             <input type="checkbox" name="ziele-der-website" value="auffindbarkeit-erhöhen" id='auffindbarkeit-erhöhen' />
+            <label htmlFor="auffindbarkeit-erhöhen"> Auffindbarkeit in meiner Umgebung erhöhen</label>
+         </div>
+         <div>
+             <input type="checkbox" name="ziele-der-website" value="mitarbeiter-gewinnen" id='mitarbeiter-gewinnen' />
+            <label htmlFor="mitarbeiter-gewinnen"> Mitarbeiter gewinnen</label>
+         </div>
+         <div>
+             <input type="checkbox" name="ziele-der-website" value="andere-ziele" id='andere-ziele' />
+            <label htmlFor="andere-ziele"> Andere (möchte ich mit Ihnen besprechen)</label>
+         </div>
+
+          </div>
+          </div>
+        </div>
+
+        <div ref={cardRef} className={styles.questionContainer}>
+          
+          <div className={styles.an}>
+          <p className={styles.questionText}>Texte und Bilder für Ihre Website:</p>
+
+          <div className={`${styles.optionsContainer} ${styles.borderButtom}`}>
+
+            <div>
+              <input type="radio" name="texte-für-website"   value="werden-bereitgestellt" id="werden-bereitgestellt" />
+               <label htmlFor="werden-bereitgestellt">Texte werden bereitgestellt</label>
+            </div>
+
+            <div>
+               <input type="radio" name="texte-für-website"    value="durch-designer" id="durch-designer"/>
+                <label htmlFor="durch-designer">Designer soll die Texte erstellen</label>
+            </div>
+             
+          </div>
+
+
+         
+
+          <div className={styles.optionsContainer}>
+
+            <div>
+            <input type="radio" name="bilder-für-website" value="werden-bereitgestellt" id="bilder-werden-bereitgestellt" />
+             <label htmlFor="bilder-werden-bereitgestellt">Bilder werden bereitgestellt</label>
+            </div>
+
+            <div>
+            <input type="radio" name="bilder-für-website" value="durch-designer" id="bilder-durch-designer"/>
+             <label htmlFor="bilder-durch-designer">Designer soll die Bilder erstellen / Kaufen</label>
+
+            </div>
+            
+
+            
+          </div>
+
+          
+          </div>
+        </div>
+
+        <div ref={cardRef} className={styles.questionContainer}>
+          
+          <div className={styles.an}>
+         
+          <p className={styles.questionText}>Gleich geschafft! Bitte geben Sie Ihre Kontaktdaten ein:</p>
+          
+          <div className={styles.spacingButtom}>
+          <input
+           className={styles.textInput}
+            type="email"
+            name="email"
+           /*  value={formData.branche}
+            onChange={handleInputChange} */
+            placeholder=' Beispiel@gmail.com'
+          />
+
+          </div>
+
+          <div>
+          <input
+            className={styles.textInput}
+            type="tel"
+            name="number"
+           /*  value={formData.branche}
+            onChange={handleInputChange} */
+            placeholder='Telefonnummer'
+            required
+          />
+          </div>
+         
+        
+          </div>
+        </div>
+
+  
       </div>
 
       <div className={styles.cFooter}>
         <div className={styles.buttonsContainer}>
-             <button className={styles.btnLeft} 
+             <button 
+             disabled={scrollPosition === 0}
+              className={`${styles.btnLeft} ${ 
+             scrollPosition === 0 ? styles.disabled : ''
+            }`} 
              onClick={handleScrollLeft}>
-             &lt;
+               <span className={styles.iconC}>
+               <BsArrowLeftSquareFill  />
+               </span>
+             Zurück
              </button>
              <button className={styles.btnRight} 
              onClick={handleScrollRight}>
-             &gt;
+             Weiter
+             <span className={styles.iconC}>
+               <BsArrowRightSquareFill  />
+               </span>
+             
            </button>
         </div>
       </div>
