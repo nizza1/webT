e308f2f521e38f11ccd61d00d9ac8b4c6871ace7
nizar <nizardjassim@gmail.com>
2024-01-25 14:55:03 +0100

    about & ilustration_contact

--- src/app/components/about/about.jsx
+++ src/app/components/about/about.jsx
@@ -12,9 +12,10 @@ import styles from './styles.module.css'
 const nizarImage = '/images/pMe.jpg'
 const dashaImage = '/dasha.jpeg' */
 
-import chrisImage from 'public/chrisBohdan.jpeg'
-import nizarImage from 'public/images/pMe.jpg'
-import dashaImage from 'public/dasha.jpeg'
+import chrisImage from 'public/up-Chris.jpg'
+import nizarImage from 'public/up-Nizar.jpg'
+import dashaImage from 'public/up-Sofia.jpg'
+import alexImage from 'public/up-Alex.jpg'
 
 import Overlay from '../globalComponents/overlay/overlay'
 
@@ -25,6 +26,7 @@ const About = () => {
 
       <Overlay />
       <div className={styles.overlay}></div>
+
        <div className={styles.innerContainer}>
        
        <div className={styles.imgTextContainer}>
@@ -35,9 +37,9 @@ const About = () => {
         </div>
 
         <div className={styles.aboutTextContainer}>
-            <h2 className={styles.aboutText}>
+            <p className={styles.aboutText}>
              In der Welt des digitalen Designs und der Webentwicklung steht Nuancestudio für Qualität, Innovation und maßgeschneiderte Lösungen. <br /> Unser Team bringt eine vielfältige Palette an Fähigkeiten und Fachwissen zusammen, um Projekte zu realisieren, die nicht nur beeindrucken, sondern auch funktionale Exzellenz bieten.
-            </h2>
+            </p>
 
           {/*    <h2 className={styles.titleB}>Wir haben das Know-how. <br />Sie haben die Ziele.</h2> */}
              <p><Title text='Wir haben das Know-how.'/>
@@ -47,98 +49,133 @@ const About = () => {
 
         </div>
 
-        <div className={styles.allImages}>
-
-        <div className={styles.singleAbout}>
-         
-         <div className={styles.imgContainer}>
-           <Image
-           className={styles.img}
-           src={nizarImage}
-           fill={true}
-           placeholder='blur'
-           alt='Nizar Djassim'
-           style={{
-             objectFit: 'cover',
-           }}
-           />
-         </div>
-         <div className={styles.underImage}>
-         <div className={styles.uberMichText}>
-          <p className={styles.imgTitle}>Nizar  </p>
-          <p className={styles.imgDes}>
-            "Webentwickler" <br /> Ich sorge dafür, dass Ihre digitale Präsenz nicht nur optisch ansprechend, sondern auch technisch einwandfrei ist.</p>
-        
-          </div>
-         </div>
-        
-       </div>
-
-       <div className={styles.singleAbout}>
-         <div className={styles.imgContainer}>
-           <Image
-           className={styles.img}
-           src={chrisImage}
-         /*   width={400}
-           height={500} */
-           placeholder='blur'
-           fill={true}
-           alt='chris Bohdan'
-           style={{
-             objectFit: 'cover',
-           }}
-           />
-         </div>
-         <div className={styles.underImage}>
-          <div className={styles.uberMichText}>
-          <p className={styles.imgTitle}>Christian  </p>
-          <p className={styles.imgDes}> "Marketing und Kundensupport" <br />Ich bin Ihr erster Ansprechpartner und sorge dafür, dass Ihre Bedürfnisse verstanden und erfüllt werden.</p>
-        
-          </div>
-       
-      
-         </div>
-         
-       
-       </div>
-
-       <div className={styles.singleAbout}>
-         <div className={styles.imgContainer}>
-           <Image
-           className={styles.img}
-           src={dashaImage}
-         /*   width={400}
-           height={500} */
-           placeholder='blur'
-           fill={true}
-           alt='Sofia'
-           style={{
-             objectFit: 'cover',
-           }}
-           />
-         </div>
-         <div className={styles.underImage}>
-         <div className={styles.uberMichText}>
-          <p className={styles.imgTitle}>Sofia  </p>
-          <p className={styles.imgDes}>
-            "Grafik und Webdesign" <br />
-            Unsere kreative Kraft im Grafikdesign.<br /> Sofia bringt Ideen zum Leben und sorgt dafür, dass jedes Projekt visuell heraussticht.</p>
-        
-          </div>
-         </div>
-         
-       
-       </div>
-
-
-       
-        </div>
-
-
-
     
         </div>
+
+        <div className={styles.imagesContAll}>
+           <div className={styles.allImages}>
+
+<div className={styles.singleAbout}>
+ 
+ <div className={styles.imgContainer}>
+   <Image
+   className={styles.img}
+   src={nizarImage}
+   fill={true}
+   placeholder='blur'
+   alt='Nizar Djassim'
+   style={{
+     objectFit: 'cover',
+   }}
+   />
+ </div>
+ <div className={styles.underImage}>
+ <div className={styles.uberMichText}>
+  <p className={styles.imgTitle}>Nizar  </p>
+  <p className={styles.imgDes}>
+    "Webentwickler" <br /> Ich sorge dafür, dass Ihre digitale Präsenz nicht nur optisch ansprechend, sondern auch technisch einwandfrei ist.</p>
+
+  </div>
+ </div>
+
+</div>
+
+<div className={styles.singleAbout}>
+ <div className={styles.imgContainer}>
+   <Image
+   className={styles.img}
+   src={chrisImage}
+ /*   width={400}
+   height={500} */
+   placeholder='blur'
+   fill={true}
+   alt='chris Bohdan'
+   style={{
+     objectFit: 'cover',
+   }}
+   />
+ </div>
+ <div className={styles.underImage}>
+  <div className={styles.uberMichText}>
+  <p className={styles.imgTitle}>Christian  </p>
+  <p className={styles.imgDes}> "Marketing und Kundensupport" <br />Ich bin Ihr erster Ansprechpartner und sorge dafür, dass Ihre Bedürfnisse verstanden und erfüllt werden.</p>
+
+  </div>
+
+
+ </div>
+ 
+
+</div>
+
+
+           </div>
+
+            <div className={styles.allImages}>
+<div className={styles.singleAbout}>
+ <div className={styles.imgContainer}>
+   <Image
+   className={styles.img}
+   src={dashaImage}
+ /*   width={400}
+   height={500} */
+   placeholder='blur'
+   fill={true}
+   alt='Sofia'
+   style={{
+     objectFit: 'cover',
+   }}
+   />
+ </div>
+ <div className={styles.underImage}>
+ <div className={styles.uberMichText}>
+  <p className={styles.imgTitle}>Sofia  </p>
+  <p className={styles.imgDes}>
+    "Grafik und Webdesign" <br />
+    Unsere kreative Kraft im Grafikdesign.<br /> Sofia bringt Ideen zum Leben und sorgt dafür, dass jedes Projekt visuell heraussticht.</p>
+
+  </div>
+ </div>
+ 
+
+</div>
+
+<div className={styles.singleAbout}>
+ <div className={styles.imgContainer}>
+   <Image
+   className={styles.img}
+   src={alexImage}
+ /*   width={400}
+   height={500} */
+   placeholder='blur'
+   fill={true}
+   alt='Sofia'
+   style={{
+     objectFit: 'cover',
+   }}
+   />
+ </div>
+ <div className={styles.underImage}>
+ <div className={styles.uberMichText}>
+  <p className={styles.imgTitle}>Alex  </p>
+  <p className={styles.imgDes}>
+    "Copywriter" <br />
+    
+    Ich verleihe Ihren Ideen überzeugende Worte. Meine Mission ist es, Ihre Botschaft prägnant und wirkungsvoll zu formulieren</p>
+
+  </div>
+ </div>
+ 
+
+</div>
+
+           </div>
+          
+        </div>
+
+
        </div>
+
        </div>
     </div>
   )
